299872e19a98450b5760c74c9ab1e75ad7c8c65f
Christopher Wales <cjwales@gmail.com>
2019-10-02 16:52:10 +0100

    mvp & extension

---
+++ models/traveller.js
@@ -0,0 +1,52 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  let result = this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
+  return result;
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  let result = this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
+  return result;
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  let result = this.journeys.filter((journey) => {
+    if (journey.transport === transport) {
+      return journey;
+    }
+  });
+  return result;
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  let result = this.journeys.filter((journey) => {
+    if (journey.distance > minDistance) {
+      return journey;
+    }
+  });
+  return result;
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  let total = this.journeys.reduce((runningTotal, journey) => {
+    return runningTotal += journey.distance;
+  }, 0);
+  return total;
+};
+
+// Traveller.prototype.getUniqueModesOfTransport = function () {
+//   let transportArray = this.journeys.map((journey) => {
+//     return journey.transport;
+//   });
+//   transportArray.filter()
+// };
+
+
+module.exports = Traveller;
